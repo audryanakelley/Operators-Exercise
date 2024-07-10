cc1b57ada0cf207318574cf011f7afa4d3f27869
Audryana Kelley <audryanakelley@gmail.com>
2024-07-10 13:06:25 -0500

    project updates

--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -2,9 +2,32 @@
 {
     public class Program
     {
-        static void Main(string[] args)
+         
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+          int a = 17;
+          int b = 4;
+          int sum = a + b; 
+          int quotient = a / b;
+          int remainder = a % b;
+
+
+          Console.WriteLine(a);
+          Console.WriteLine(b);
+
+          Console.WriteLine($"{a}/{b} is {quotient} remainder {remainder}.");
+          
+          Console.Write("Enter the radius of the circle: ");
+          double radius = double.Parse(Console.ReadLine());
+          double area = AreaOfCircle(radius);
+          Console.WriteLine($"The area of the circle with radius {radius} is {area}");
         }
-    }
+        
+        static double AreaOfCircle(double radius)
+        {
+            return Math.PI * Math.Pow(radius, 2);
+        }
+        
+        
+    }   
 }
